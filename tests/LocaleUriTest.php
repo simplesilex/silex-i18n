889daef12c82f4a5af330e20e433001140685137
davletshin <yuriy.davletshin@gmail.com>
2014-05-06 14:57:53 +0300

    Added empty test

---
+++ tests/LocaleUriTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SimpleSilex\SilexI18n\Tests;
+
+class LocaleUriTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function testGettingUri()
+    {
+    }
+}
