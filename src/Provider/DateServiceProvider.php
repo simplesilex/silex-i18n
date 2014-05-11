a453fb908725fddeab8a58e3c5bb627456bb7ed7
davletshin <yuriy.davletshin@gmail.com>
2014-05-11 23:36:01 +0300

    Added lists of locales

--- src/Provider/DateServiceProvider.php
+++ src/Provider/DateServiceProvider.php
@@ -25,6 +25,44 @@ class DateServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
+        /**
+         * Configures this provider
+         */
+        $app['system_locales'] = array(
+            'en-US' => array(
+                'name' => 'American',
+                'datetime' => 'n/j/Y H:i:s',
+                'short_date' => 'n/j/y', // 4/17/14
+                'medium_date' => 'M j, Y', // Apr 17, 2014
+                'long_date' => 'F j, Y', // April 17, 2014
+                'full_date' => 'l, F j, Y', //Thursday, April 17, 2014
+            ),
+            'en' => array(
+                'name' => 'English',
+                'datetime' => 'd/m/Y H:i:s',
+                'short_date' => 'd/m/y', // 17/04/14
+                'medium_date' => 'd-M-Y', // 17-Apr-2014
+                'long_date' => 'j F Y', // 17 April 2014
+                'full_date' => 'j F Y', // 17 April 2014
+            ),
+            'de' => array(
+                'name' => 'Deutsch',
+                'datetime' => 'd.m.Y H:i:s',
+                'short_date' => 'd.m.y', // 17.04.14
+                'medium_date' => 'd.m.Y', // 17.04.2014
+                'long_date' => 'j. F Y', // 17. April 2014
+                'full_date' => 'l, j. F Y', // Donnerstag, 17. April 2014
+            ),
+            'fr' => array(
+                'name' => 'Français',
+                'datetime' => 'd/m/Y H:i:s',
+                'short_date' => 'm/d/y', // 17/04/14
+                'medium_date' => 'd M. Y', // 17 Apr. 2014
+                'long_date' => 'j F Y', // 17 Avril 2014
+                'full_date' => 'l j F Y', // Jeudi 17 Avril 2014
+            ),
+        );
+        $app['locale'] = 'en';
     }
 
     /**
