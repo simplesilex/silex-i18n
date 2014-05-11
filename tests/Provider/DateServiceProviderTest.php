c0c142f636e346d6a5529cad1385dc4e38b3c10d
davletshin <yuriy.davletshin@gmail.com>
2014-05-11 22:46:05 +0300

    Added DateServiceProviderTest

---
+++ tests/Provider/DateServiceProviderTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Part of the Silex-i18n package.
+ *
+ * @package   SilexLocalizer
+ * @copyright 2014 Yuriy Davletshin
+ * @license   http://opensource.org/licenses/mit-license/ The MIT License (MIT)
+ * @link      http://www.simplesilex.com/
+ */
+
+namespace SimpleSilex\SilexI18n\Tests\Provider;
+
+use Silex\WebTestCase;
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+
+/**
+ * Tests DateServiceProvider.
+ *
+ * @author Yuriy Davletshin <yuriy.davletshin@gmail.com>
+ */
+class DateServiceProviderTest extends WebTestCase
+{
+    /**
+     * Gets an instance of Silex application.
+     *
+     * @return Application The instance of the application
+     */
+    public function createApplication()
+    {
+        /**
+         * Creates the application under test
+         */
+        $app = new Application();
+
+        /**
+         * Registers some providers
+         */
+        $app->register(new TwigServiceProvider());
+
+        /**
+         * Configures the application
+         */
+        $app['debug'] = true;
+        $app['exception_handler']->disable();
+
+        $app['twig.path'] = __DIR__ . '/../templates';
+
+        return $app;
+    }
+
+    /**
+     * Tests the Silex application.
+     */
+    public function testInitApplication()
+    {
+        $this->assertEquals(get_class($this->app), 'Silex\Application');
+    }
+
+    /**
+     * Tests the Twig Environment.
+     */
+    public function testTwigEnvironment()
+    {
+        $this->assertEquals(get_class($this->app['twig']), 'Twig_Environment');
+    }
+}
