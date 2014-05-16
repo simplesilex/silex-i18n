1c0f7869c69de822ec6c12df123a7720eba0f9bb
davletshin <yuriy.davletshin@gmail.com>
2014-05-16 14:52:10 +0300

    Updated DateServiceProvider

--- src/Provider/DateServiceProvider.php
+++ src/Provider/DateServiceProvider.php
@@ -29,40 +29,17 @@ class DateServiceProvider implements ServiceProviderInterface
          * Configures this provider
          */
         $app['system_locales'] = array(
-            'en-US' => array(
-                'name' => 'American',
-                'datetime' => 'n/j/Y H:i:s',
-                'short_date' => 'n/j/y', // 4/17/14
-                'medium_date' => 'M j, Y', // Apr 17, 2014
-                'long_date' => 'F j, Y', // April 17, 2014
-                'full_date' => 'l, F j, Y', //Thursday, April 17, 2014
-            ),
             'en' => array(
                 'name' => 'English',
-                'datetime' => 'd/m/Y H:i:s',
-                'short_date' => 'd/m/y', // 17/04/14
-                'medium_date' => 'd-M-Y', // 17-Apr-2014
-                'long_date' => 'j F Y', // 17 April 2014
-                'full_date' => 'j F Y', // 17 April 2014
-            ),
-            'de' => array(
-                'name' => 'Deutsch',
-                'datetime' => 'd.m.Y H:i:s',
-                'short_date' => 'd.m.y', // 17.04.14
-                'medium_date' => 'd.m.Y', // 17.04.2014
-                'long_date' => 'j. F Y', // 17. April 2014
-                'full_date' => 'l, j. F Y', // Donnerstag, 17. April 2014
-            ),
-            'fr' => array(
-                'name' => 'Français',
-                'datetime' => 'd/m/Y H:i:s',
-                'short_date' => 'd/m/y', // 17/04/14
-                'medium_date' => 'd M Y', // 17 Apr 2014
-                'long_date' => 'j F Y', // 17 Avril 2014
-                'full_date' => 'l j F Y', // Jeudi 17 Avril 2014
+                'date_time' => 'n/j/Y g:i:s A', // 4/17/14 4:32:12 AM
+                'short_date' => 'n/j/y',        // 4/17/14
+                'medium_date' => 'M j, Y',      // Apr 17, 2014
+                'long_date' => 'F j, Y',        // April 17, 2014
+                'full_date' => 'l, F j, Y',     // Thursday, April 17, 2014
             ),
         );
         $app['locale'] = 'en';
+        $app['locale_fallbacks'] = array($app['locale']);
 
         /**
          * Extends Twig
@@ -79,23 +56,15 @@ class DateServiceProvider implements ServiceProviderInterface
             $twig->addFilter(
                 new \Twig_SimpleFilter(
                     'localedate',
-                    function (
-                        $date,
-                        $type = null,
-                        $timezone = null
-                    ) use ($twig, $app) {
+                    function ($date, $type = null) use ($twig, $app) {
                         $locale = $app['system_locales'][$app['locale']];
                         if (null !== $type && isset($locale[$type])) {
                             $format = $locale[$type];
                         } else {
                             $format = null;
                         }
-                        return twig_date_format_filter(
-                            $twig,
-                            $date,
-                            $format,
-                            $timezone
-                        );
+                        // timezone = null. TODO: Add timezone support.
+                        return twig_date_format_filter($twig, $date, $format);
                     }
                 )
             );
